7c24078fe8d1075278d15f62b282a662e5307102
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-01-20 19:43:41 -0300

    requisito 06 em andamento

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,7 +1,52 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import renderWithRouter from '../renderWithRouter';
-// import userEvent from '@testing-library/user-event';
-// import App from '../App';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
-describe('', () => {});
+describe('Testes do componente <Pokemon.js />', () => {
+  it('Deve ser renderizado um card com as informações de determinado Pokémon', () => {
+    renderWithRouter(<App />);
+
+    const name = 'Pikachu';
+
+    const pokemonName = screen.getByText(name);
+    const pokemonType = screen.getByTestId('pokemon-type', {
+      name: /Electric/i,
+    });
+    const pokemonWeight = screen.getByText(/average weight: 6\.0 kg/i);
+
+    const pokemonImg = document.querySelector('img');
+
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonType).toBeInTheDocument();
+    expect(pokemonWeight).toBeInTheDocument();
+    expect(pokemonImg).toBeInTheDocument();
+    expect(pokemonImg.src).toBe(
+      'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png',
+    );
+    expect(pokemonImg.alt).toBe(`${name} sprite`);
+  });
+
+  it('O card do Pokémon indicado na Pokédex deve conter um link de navegação para exibir detalhes', () => {
+    renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', { name: /more details/i });
+    expect(detailsLink).toBeInTheDocument();
+
+    const URL = '/pokemon/25';
+
+    expect(detailsLink.pathname).toBe(URL);
+  });
+
+  it('ao clicar no link de navegação do Pokémon, é feito o redirecionamento da aplicação para a página de detalhes de Pokémon', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+
+    const { pathname } = history.location;
+    const URL = '/pokemon/25';
+    expect(pathname).toBe(URL);
+  });
+});
